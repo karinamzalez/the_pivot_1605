96ab625430c66fc0bcb710438e42cb66f0d57481
Karina Gonzalez <karinamzalez@gmail.com>
2016-08-16 15:03:29 -0600

    change existing schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803162238) do
+ActiveRecord::Schema.define(version: 20160816204058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,26 +22,26 @@ ActiveRecord::Schema.define(version: 20160803162238) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "gnomes", force: :cascade do |t|
+  create_table "items", force: :cascade do |t|
     t.string  "name"
     t.decimal "price"
-    t.text    "desc"
+    t.text    "description"
     t.integer "category_id"
     t.string  "image_url"
-    t.integer "role"
+    t.boolean "status",      default: false
   end
 
-  add_index "gnomes", ["category_id"], name: "index_gnomes_on_category_id", using: :btree
+  add_index "items", ["category_id"], name: "index_items_on_category_id", using: :btree
 
-  create_table "order_gnomes", force: :cascade do |t|
-    t.integer "gnome_id"
+  create_table "order_items", force: :cascade do |t|
     t.integer "order_id"
     t.integer "quantity"
     t.decimal "subtotal"
+    t.integer "item_id"
   end
 
-  add_index "order_gnomes", ["gnome_id"], name: "index_order_gnomes_on_gnome_id", using: :btree
-  add_index "order_gnomes", ["order_id"], name: "index_order_gnomes_on_order_id", using: :btree
+  add_index "order_items", ["item_id"], name: "index_order_items_on_item_id", using: :btree
+  add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
     t.decimal  "total_price"
@@ -56,22 +56,14 @@ ActiveRecord::Schema.define(version: 20160803162238) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.integer  "role",               default: 0
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "role",            default: 0
     t.string   "email"
-    t.string   "country_code"
-    t.string   "phone_number"
-    t.string   "authy_id"
-    t.string   "verified",           default: "f"
   end
 
-  add_foreign_key "gnomes", "categories"
-  add_foreign_key "order_gnomes", "gnomes"
-  add_foreign_key "order_gnomes", "orders"
+  add_foreign_key "items", "categories"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
 end
