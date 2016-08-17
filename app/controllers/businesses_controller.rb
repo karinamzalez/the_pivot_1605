687363fba8af393609826a6ec20a75031c6b0c3b
Karina Gonzalez <karinamzalez@gmail.com>
2016-08-17 11:40:33 -0600

    pulled from updated dev branch

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,8 +1,9 @@
 class BusinessesController < ApplicationController
   def index
     @businesses = Business.all
+    @categories = Category.all 
   end
-  
+
   def show
     @business = Business.find(params[:id])
   end
