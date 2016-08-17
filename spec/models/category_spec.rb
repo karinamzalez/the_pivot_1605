a4b581bd57e119dc680b539d53da3c907ea009a1
Zack Forbing <zackforbing@gmail.com>
2016-08-17 11:00:52 -0600

    Update category_spec.rb
    removed FactoryGirl from create

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it "has a valid factory" do
-    FactoryGirl.create(:category).should be_valid
+    create(:category).should be_valid
   end
 end
