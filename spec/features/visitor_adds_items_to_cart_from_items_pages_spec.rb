be801410b47549b956b2dc6e31dc081d65b53bc8
Karina Gonzalez <karinamzalez@gmail.com>
2016-08-18 11:45:26 -0600

    deleted comments from visitor adds items to cart

--- spec/features/visitor_adds_items_to_cart_from_items_pages_spec.rb
+++ spec/features/visitor_adds_items_to_cart_from_items_pages_spec.rb
@@ -5,15 +5,13 @@ RSpec.feature "Visitor cart additions" do
   context "Multiple items are available for purchase" do
     seed_test_data
     scenario "visitor views item #1 and adds it to their cart" do
-      # as a visitor
-      # when I view an item's page
+
       visit item_path(Item.all.first)
-      # and I click add to cart
+
       click_button("Add to Cart")
-      
-      # I see a message that the item was added to my cart
+
       expect(page).to have_content(" has been added to your cart")
-      # and my cart is incremented by 1
+
       expect(page).to have_content("Cart: 1")
     end
   end
