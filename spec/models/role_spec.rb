29888978e2c3a277c5cb09997d6bb94368eea894
Kris Sparks <kbs5280@gmail.com>
2016-08-21 11:52:14 -0600

    Role spec passes

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_uniqueness_of(:name) }
+
+  it do
+    should have_many(:user_roles)
+  end
+
+  it do
+    should have_many(:roles).
+      through(:user_roles).
+      class_name('Role')
+  end
 end
