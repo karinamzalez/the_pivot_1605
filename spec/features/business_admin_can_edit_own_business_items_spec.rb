cfe1e47830e7961cf6cdde94190d9ca0769c8533
Karina Gonzalez <karinamzalez@gmail.com>
2016-08-22 14:48:12 -0600

    change way relationship bw admin and business is established

--- spec/features/business_admin_can_edit_own_business_items_spec.rb
+++ spec/features/business_admin_can_edit_own_business_items_spec.rb
@@ -4,17 +4,17 @@ include TestHelper
 
 describe 'Businesss admin can edit own business items', type: :feature do
   scenario 'and views updated items in dashboard' do
-    admin = create(:user)
+    business = create(:business)
+    admin = create(:user, business_id: business.id)
     create_roles
     admin.set_business_admin
-    business = create(:business)
-    require "pry"; binding.pry
-    admin.business_id = business.id
     create_test_business_items
     page.set_rack_session(user_id: admin.id)
 
     visit dashboard_path
 
-    expect(page).to have_content(business.name)
+    within("p") do
+      expect(page).to have_content(business.name)
+    end
   end
 end
