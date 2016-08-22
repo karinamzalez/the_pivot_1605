ffcb9df1a27ebecff0bea5cb37c674ac3e2db824
Karina Gonzalez <karinamzalez@gmail.com>
2016-08-22 12:00:03 -0600

    details of business.js

--- app/assets/javascripts/businesses.js
+++ app/assets/javascripts/businesses.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-
-  function toggleMenu (buttonID, listID){
-    $(buttonID).on("click", function(){
-      $(listID).toggleClass("hidden")
-    });
-  };
-
-  toggleMenu("#online", "#all")
-  toggleMenu("#categories", "#more")
+  toggleMenu("#online", "#all");
+  toggleMenu("#categories", "#more");
 });
+
+function toggleMenu (buttonID, listID){
+  $(buttonID).on("click", function(){
+    $(listID).toggleClass("hidden");
+  });
+}
