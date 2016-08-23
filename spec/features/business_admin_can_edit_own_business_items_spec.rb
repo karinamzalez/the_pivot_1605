ba39973d8b5a17525da6e71651cfb996d250f1bb
Karina Gonzalez <karinamzalez@gmail.com>
2016-08-23 09:41:46 -0600

    refactored spec with a before all action

--- spec/features/business_admin_can_edit_own_business_items_spec.rb
+++ spec/features/business_admin_can_edit_own_business_items_spec.rb
@@ -4,50 +4,50 @@ include TestHelper
 
 RSpec.feature "Business Admin Sees All Business Items" do
   context "business admin visits dashboard" do
-    scenario 'admin can view all related business items' do
-      business = create(:business)
-      admin = create(:user, business_id: business.id)
+
+    before :all do
+      @business = create(:business)
+      @admin = create(:user, business_id: @business.id)
       create_roles
-      admin.set_business_admin
+      @admin.set_business_admin(@business)
       create_test_categories
       create_test_business_items
-      page.set_rack_session(user_id: admin.id)
+    end
 
+    scenario 'admin can view all related business items' do
+      page.set_rack_session(user_id: @admin.id)
       visit dashboard_path
 
       within("p.name") do
-        expect(page).to have_content(business.name)
+        expect(page).to have_content(@business.name)
       end
 
-      click_on("View #{business.name} Items")
+      click_on("View #{@business.name} Items")
 
-      expect(page).to have_content(business.items.first.name)
-      expect(page).to have_content(business.items.last.name)
+      expect(page).to have_content(@business.items.first.name)
+      expect(page).to have_content(@business.items.last.name)
     end
 
     scenario "business admin can edit business items" do
-      business = create(:business)
-      admin = create(:user, business_id: business.id)
-      create_roles
-      admin.set_business_admin
-      create_test_categories
-      create_test_business_items
-      page.set_rack_session(user_id: admin.id)
-
+      page.set_rack_session(user_id: @admin.id)
       visit dashboard_path
 
-      click_on("View #{business.name} Items")
+      click_on("View #{@business.name} Items")
 
-      within("tr.#{business.items.first.slug}") do
+      within("tr.#{@business.items.first.slug}") do
         click_on("Edit")
 
-        expect(current_path).to eq("/admin/items/#{business.items.first.slug}/edit")
+        expect(current_path).to eq("/admin/items/#{@business.items.first.slug}/edit")
 
-        fill_in "price",  with: "apple"
+        fill_in "item_name",  with: 'apple'
         click_on("Update Item")
 
-        expect(business.items.first.name).to eq("apple")
+        expect(@business.items.first.name).to eq("apple")
       end
+    end
+
+    scenario "business admin can delete business items" do
+      page.set_rack_session(user_id: @admin.id)
     end
   end
 end
