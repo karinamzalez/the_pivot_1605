19cadcabc4998f9e6f81549e2d6af7e239aa8ea3
Kris Sparks <kbs5280@gmail.com>
2016-08-23 14:14:34 -0600

    Add test for removes business admin NOT WORKING
    remove_business_admin does not appear to be deleting record from the database. Not sure
    why. Tried destroy, destroy_all, delete, delete_all, looked at validation. Please ensure
    that the test is written properly. Currently commented out in user_spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,10 +24,15 @@ describe User, type: :model do
   end
 
   it 'checks registered user ' do
+    user = create(:user, :as_registered_user)
+    expect(user.registered_user?).to eq(true)
+  end
+
+  it 'sets platform_admin' do
     user = create(:user)
     create_roles
-    user.set_registered_user
-    expect(user.registered_user?).to eq(true)
+    user.set_platform_admin
+    expect(user.roles.first.name).to eq('platform_admin')
   end
 
   it 'sets business admin' do
@@ -38,4 +43,19 @@ describe User, type: :model do
     expect(user.roles.first.name).to eq('business_admin')
     expect(user.business.name).to eq(business.name)
   end
+
+  it 'sets registered_user' do
+    user = create(:user)
+    create_roles
+    user.set_registered_user
+    expect(user.roles.first.name).to eq('registered_user')
+  end
+
+  # it 'removes business admin' do
+  #   user = create(:user, :as_business_admin)
+  #   user.remove_business_admin
+  #
+  #   expect(user.roles).to eq(nil)
+  #   expect(user.business_id).to eq(nil)
+  # end
 end
