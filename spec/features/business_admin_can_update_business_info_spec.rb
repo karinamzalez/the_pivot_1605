9131f30a8249e0f883340ec78a5afd0906cd1f1d
Zack Forbing <zackforbing@gmail.com>
2016-08-22 18:44:14 -0600

    add business info update sad path test

--- spec/features/business_admin_can_update_business_info_spec.rb
+++ spec/features/business_admin_can_update_business_info_spec.rb
@@ -20,4 +20,21 @@ describe 'Business admin can update business information', type: :feature do
     expect(page).to have_content(updated_business_name)
     expect(page).to have_content(updated_business_address)
   end
+
+  scenario 'admin cannot update info with blank fields' do
+    business = create(:business)
+    role = Role.create(name: 'business_admin')
+    user = create(:user, business_id: business.id)
+    user.roles << role
+    page.set_rack_session(user_id: user.id)
+
+    visit dashboard_path
+
+    click_link 'Update Business Info'
+    fill_in 'Business Name', with: 'blah incorporated'
+        fill_in 'Location', with: ''
+    click_button 'Update Business Info'
+    expect(page).to have_content('Business information not updated successfully.')
+    expect(current_path).to eq (edit_business_path(business))
+  end
 end
