4f7db56e3a38f2da2b7e38d1fbeea81261963dd6
Zack Forbing <zackforbing@gmail.com>
2016-08-24 19:11:58 -0600

    add tests, refactor, validations

--- spec/features/business_admin_can_edit_own_business_items_spec.rb
+++ spec/features/business_admin_can_edit_own_business_items_spec.rb
@@ -21,7 +21,6 @@ RSpec.feature "Business Admin Sees All Business Items" do
       within("p.name") do
         expect(page).to have_content(@business.name)
       end
-
       click_on("View #{@business.name} Items")
 
       expect(page).to have_content(@business.items.first.name)
@@ -30,10 +29,10 @@ RSpec.feature "Business Admin Sees All Business Items" do
 
     scenario "business admin can edit business items" do
       page.set_rack_session(user_id: @admin.id)
+
       visit dashboard_path
 
       click_on("View #{@business.name} Items")
-
       within("tr.#{@business.items.first.slug}") do
         click_on("Edit")
       end
@@ -48,10 +47,10 @@ RSpec.feature "Business Admin Sees All Business Items" do
 
     scenario "business admin can delete business items" do
       page.set_rack_session(user_id: @admin.id)
+
       visit dashboard_path
 
       click_on("View #{@business.name} Items")
-
       within("tr.#{@business.items.first.slug}") do
         click_on("Remove")
       end
@@ -61,20 +60,57 @@ RSpec.feature "Business Admin Sees All Business Items" do
 
     scenario "business admin can create new business item" do
       page.set_rack_session(user_id: @admin.id)
+
       visit dashboard_path
 
       click_on("View #{@business.name} Items")
-
       click_on("New Item")
 
       expect(current_path).to eq("/admin/items/new")
 
       fill_in "Name",  with: 'orange'
+      fill_in "Price",  with: 42
+      fill_in "Description",  with: "I mean, it's an orange."
+      fill_in "Image url",  with: 'hi'
       click_on "Create Item"
 
       expect(current_path).to eq("/dashboard")
-
       expect(page).to have_content("orange")
+    end
+
+    scenario "business admin can't create new business item without a name" do
+      page.set_rack_session(user_id: @admin.id)
+
+      visit dashboard_path
+
+      click_on("View #{@business.name} Items")
+      click_on("New Item")
+
+      expect(current_path).to eq("/admin/items/new")
+
+      fill_in "Price",  with: 42
+      fill_in "Description",  with: "I mean, it's an orange."
+      fill_in "Image url",  with: 'hi'
+      click_on "Create Item"
+
+      expect(page).to have_content("Name can't be blank")
+    end
+    scenario "business admin can't edit business items without correct formatting" do
+      page.set_rack_session(user_id: @admin.id)
+
+      visit dashboard_path
+
+      click_on("View #{@business.name} Items")
+      within("tr.#{@business.items.first.slug}") do
+        click_on("Edit")
+      end
+
+      expect(current_path).to eq("/admin/items/#{@business.items.first.slug}/edit")
+
+      fill_in "Price",  with: 'apple'
+      click_on("Update Item")
+
+      expect(page).to have_content("Price is not a number")
     end
   end
 end
